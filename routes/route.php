c6266d0c1b6cac2cb1168f7baf6455784aeb87c2
Adekunle Amos Olarinde <your-email@example.com>
2026-07-06 11:49:10 +0100

    Built a micro framework for services

--- routes/route.php
+++ routes/route.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Controllers\UserController;
 use App\Http\Response;
+use App\Controllers\CategoryController;
 use App\Middleware\AuthMiddleware;
 
+$router->addRoute('GET', '/', function($request) {
+    return new Response(200, ['message' => 'Inventory Service']);
+});
 
-$router->addRoute('GET', '/users', [UserController::class, 'index']);
-$router->addRoute('GET', '/user/{id}', [UserController::class, 'show']);
-
-$router->addRoute('POST', '/user', [UserController::class, 'store'], [AuthMiddleware::class]);
-$router->addRoute('PUT', '/user/{id}', [UserController::class, 'update'], [AuthMiddleware::class]);
-$router->addRoute('DELETE', '/user/{id}', [UserController::class, 'delete'], [AuthMiddleware::class]);
+$router->addRoute('GET',  '/categories',    [CategoryController::class, 'index']);
+$router->addRoute('GET',  '/category/{id}', [CategoryController::class, 'show']);
+$router->addRoute('POST', '/category',      [CategoryController::class, 'store'],  [AuthMiddleware::class]);
+$router->addRoute('PUT',  '/category/{id}', [CategoryController::class, 'update'], [AuthMiddleware::class]);
